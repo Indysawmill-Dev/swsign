fce92c9847fa72908f60e44b7125e30e50c08963
Cale H <indysawmill@gmail.com>
2020-03-24 11:21:54 -0400

    Added clock line Added comments

--- runscreen.sh
+++ runscreen.sh
@@ -2,8 +2,12 @@
 
 # MCP to run RGB LED Matrix.  This should be ran at startup, and checked from CRON
 
+# Starts the persistent clock
+  ./projects/rpi-rgb-led-matrix/examples-api-use/clock --led-cols=64 -C 38,72,63 -f ../fonts/8x13B.bdf -d '%a %b%d %l:%M:%S%p' --led-slowdown-gpio=3 -y 2 --led-chain=3 -x 16
+
+# Main loop to run regular programming
 while true ; do 
   while read line; do 
-    ./scrolling-text-example --led-cols=64 --led-chain=3 --led-slowdown-gpio=3 -y 17 -f ../fonts/9x15.bdf -C 255,0,255 -l 1 $line
+    ./projects/rpi-rgb-led-matrix/examples-api-use/scrolling-text-example --cols=64 --led-chain=3 --led-slowdown-gpio=3 -y 17 -f ../fonts/9x15.bdf -C 255,0,255 -l 1 $line
   done < test.txt 
 done
